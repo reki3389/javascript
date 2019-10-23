c803e002a459c996ce22e33cf756759338769202
Kwon <Kwon@DESKTOP-D3CCSNF>
2019-10-23 17:45:18 +0900

    gg

---
+++ javaScript/WebContent/out.js
@@ -0,0 +1,6 @@
+function Hide(){
+    document.getElementById('demo').style.display='none';
+}
+function sizeUp(){
+    document.getElementById('demo').style.fontSize='35px';
+}
---
+++ javaScript/src/emp/EmpDAO.java
@@ -0,0 +1,155 @@
+package emp;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import common.DAO;
+
+public class EmpDAO {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+
+	public void deleteEmployee(int empId) {
+		conn = DAO.getConnect();
+		String sql = "delete from emp_temp where employee_id = ?";
+		Employee emp = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, empId);
+			int rs = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public Employee getEmployee(int empId) {
+		conn = DAO.getConnect();
+		String sql = "select * from emp_temp where employee_id = ?";
+		String sql1 = "{? = call get_dept_name(?)}";
+		Employee emp = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, empId);
+			rs = pstmt.executeQuery();
+			CallableStatement cstmt = conn.prepareCall(sql1);
+			cstmt.registerOutParameter(1, java.sql.Types.VARCHAR);
+			cstmt.setInt(2, empId);
+			cstmt.execute();
+			String deptName = cstmt.getString(1);
+			
+			if (rs.next()) {
+				emp = new Employee();
+				emp.setEmployeeId(rs.getInt("employee_id"));
+				emp.setFirstName(rs.getString("first_name"));
+				emp.setLastName(rs.getString("last_name"));
+				emp.setEmail(rs.getString("email"));
+				emp.setHireDate(rs.getString("hire_date"));
+				emp.setJobId(rs.getString("job_id"));
+				emp.setSalary(rs.getInt("salary"));
+				emp.setDeptName(deptName);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return emp;
+	}
+	
+	public void insertEmpProc(Employee emp) {
+		conn = DAO.getConnect();
+		String sql = "{call add_new_member(?,?,?,?,?,?)}";
+		try {
+			CallableStatement cstmt = conn.prepareCall(sql);
+			cstmt.setString(1,  emp.getFirstName());
+			cstmt.setString(2, emp.getLastName());
+			cstmt.setString(3, emp.getJobId());
+			cstmt.setInt(4, emp.getSalary());
+			cstmt.setString(5, emp.getHireDate());
+			cstmt.setString(6, emp.getEmail());
+			cstmt.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void insertEmp(Employee emp) {
+		conn = DAO.getConnect();
+		String sql = "insert into emp_temp(employee_id, first_name, last_name, email, hire_date, job_id, salary) values (?,?,?,?,?,?,?) ";
+		int rCnt = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(++rCnt, emp.getEmployeeId()); // 첫번째 parameta
+			pstmt.setString(++rCnt, emp.getFirstName()); // 두번째
+			pstmt.setString(++rCnt, emp.getLastName());
+			pstmt.setString(++rCnt, emp.getEmail());
+			pstmt.setString(++rCnt, emp.getHireDate());
+			pstmt.setString(++rCnt, emp.getJobId());
+			pstmt.setInt(++rCnt, emp.getSalary());
+			int r = pstmt.executeUpdate();
+			System.out.println(r + "건이 입력되었습니다.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public List<Employee> getEmpList() {
+		List<Employee> list = new ArrayList<>();
+		Connection conn = DAO.getConnect();
+		String sql = "select * from emp_temp";
+		Employee emp = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				emp = new Employee();
+				emp.setEmployeeId(rs.getInt("employee_id"));
+				emp.setFirstName(rs.getString("first_name"));
+				emp.setLastName(rs.getString("last_name"));
+				emp.setHireDate(rs.getString("hire_date"));
+				emp.setEmail(rs.getString("email"));
+				emp.setSalary(rs.getInt("salary"));
+				emp.setJobId(rs.getString("job_id"));
+				list.add(emp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+}
